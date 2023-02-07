0549a0577211556abcb1bbd4c9dc2d25ddd1026c
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-02-08 02:02:47 +0530

    Formated

--- Client/src/Components/Gallery/Gallary.js
+++ Client/src/Components/Gallery/Gallary.js
@@ -3,17 +3,17 @@ import React from 'react'
 const Gallary = () => {
   return (
     <>
-       <section className=' grid-container eve-gallary'>
-    <div class="grid-element grid-elementbase">
-        <div className='font-1'>EVENTS GALLERY</div>
-        <div className='font-2'>Highlights of The Night</div>
-    </div>
+      <section className=' grid-container eve-gallary'>
+        <div class="grid-element grid-elementbase">
+          <div className='font-1'>EVENTS GALLERY</div>
+          <div className='font-2'>Highlights of The Night</div>
+        </div>
         <div class="grid-element grid-element1">1</div>
         <div class="grid-element grid-element2">2</div>
         <div class="grid-element grid-element3">3</div>
         <div class="grid-element grid-element4">4</div>
-        
-    </section>
+
+      </section>
     </>
   )
 }
