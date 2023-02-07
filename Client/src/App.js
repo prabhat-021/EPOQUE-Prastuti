39d479273863f4bba1cbdaa56637baaf35ac340c
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2023-02-07 23:51:54 +0530

    parallax added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,9 @@
-
+import Home from "./pages/Home/Home";
+import './index.css'
 export default function App() {
   return (
     <div className="App">
-     
+     <Home/>
     </div>
   );
 }
---
+++ Client/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+const Home = () =>{
+return(
+    <>
+    <section className='bg-1 sec'></section>
+    <section className='bg-2 sec'></section>
+    <section className='bg-3 sec'></section>
+    <section className='bg-4 sec'></section>
+    <section className='bg-5 sec'></section>
+    <section className='bg-6 sec'></section>
+    </>
+)
+
+}
+
+export default Home;
