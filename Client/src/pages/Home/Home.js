4d188b21c6a918e29a195d3a2d749a47ff896c27
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2023-02-08 01:40:55 +0530

    gallary section added

--- Client/src/pages/Home/Home.js
+++ Client/src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Gallary from '../../components/Gallery/Gallary';
 import './Home.css';
 const Home = () =>{
 return(
     <>
     <section className='bg-1 sec'></section>
     <section className='bg-2 sec'></section>
-    <section className='bg-3 sec'>
+    <section className=' sec-new bg-3 sec'>
         
    <div className='sec-eve-day'>Friday</div>
    <div className='sec-eve-day' >2/3/2022</div>
@@ -13,14 +14,14 @@ return(
    <div className='sec-det-eve'>DJ SUPBASS / GROOVES / DJ ROOTBOY</div>
    <button className='btn' >Get Ticket</button>
     </section>
-    <section className='bg-4 sec'>
+    <section className='sec-new bg-4 sec'>
     <div className='sec-eve-day'>SATURDAY</div>
    <div className='sec-eve-day' >22/10/2016</div>
    <div className='sec-eve-nam'>BIG CITY LIGHTS PARTY</div>
    <div className='sec-det-eve'>DJ SUPBASS / GROOVES / DJ ROOTBOY</div>
    <button className='btn' >Get Ticket</button>
     </section>
-    <section className='bg-5 sec'>
+    <section className='sec-new bg-5 sec'>
     <div className='sec-eve-day'>SUNDAY</div>
    <div className='sec-eve-day' >23/10/2016</div>
    <div className='sec-eve-nam'>COCKTAIL LONG NIGHT PARTY</div>
@@ -28,7 +29,14 @@ return(
    <button className='btn' >Get Ticket</button>
     </section>
     <section className='bg-6 sec'></section>
-    <section className='eve-gallary'></section>
+
+   <section><Gallary/></section>
+
+    <section className='sec sec-btn'>
+        <button className='btn'>ALL GALARY</button>
+    </section>
+    <section className='bg-6 sec'></section>
+    <section className='bg-6 sec'></section>
     </>
 )
 
