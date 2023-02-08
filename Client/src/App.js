2cc3092967ee1b11b7fb2f43751fc9eed5fab35b
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-02-09 00:59:44 +0530

    About Prastuti

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home/Home";
 import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutKiet from "./pages/About/AboutKiet.js";
+import AboutPrastuti from "./pages/About/AboutPrastuti.js";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutKiet" element={<AboutKiet />} />
-        {/* <Route path="/aboutPrastuti" element={<AboutPrastuti />} /> */}
+        <Route path="/aboutPrastuti" element={<AboutPrastuti />} />
         {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
