e79ac5aeebd18d48bb869e38bcc904cf62b9c871
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-02-10 17:54:26 +0530

    event data updated

--- Client/public/event.js
+++ Client/public/event.js
@@ -3,40 +3,40 @@
     {
         "id": 1,
         "image": "../../assets/images/eve1.jpg",
-        "title": "BAND WAR",
+        "title": "BATTLE OF BANDS",
         "discription": "POP , R&B , Soul and Rock, What's your genre? Prastuti's 23 presents to you the biggest platform for your band to showcase your talent among the youth of the nation. Through BATTLE OF BANDS ,It's time for you to witness a musical a musical showdown lke no other!",
         "featured":true
     },
     {
         "id": 2,
-        "image": "image1",
-        "title": "RIWAYAT 2",
+        "image": "../../assets/images/riwayat.HEIC",
+        "title": "RIWAYAT",
         "discription": "Who says fashion can't be fun? A fashion even like no other is what we call RIWAYAT.Come and witness the latest trends in fashion and enjoy a night filled with colours in vogue and high style. Join us for this event where the models ace the runway in the most attractive and confident manner.",
         "featured":true
     },
     {
         "id": 3,
         "image": "image1",
-        "title": "SIR-A-SAAJ",
-        "discription": "Get ready to unveil the singing talent of your group as Prastuti'23 presents सुसुसुर सुर-ए Saaz, a group singing event that will make you sing in rhythm and unison. Imagine a room filled with off-key notes,exaggerated harmonies, and enthusiastic performances,all coming together to create a cacophony of musical joy",
+        "title": "सुर-ए-SAAZ",
+        "discription": "Get ready to unveil the singing talent of your group as Prastuti'23 presents 'सु सुरए-Saaz' a group singing event that will make you sing in rhythm and unison. Imagine a room filled with off-key notes, exaggerated harmonies,and enthusiastic performances, all coming together to create a cacophony of musical joy.",
         "featured":true
     },
     {
         "id": 4,
         "image": "image1",
-        "title": "Heading 4",
-        "discription": "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words"
+        "title": "PRACHAND",
+        "discription": "Come adorn your character, bring your instruments, raise your voice and be ready to portray what you want to show to the crowd. Its time to shake the audience through your noble messages for the society, impress the jury, and claim the first winning prize of 'Prachand' the Nukkad Natak."
     },
     {
         "id": 5,
         "image": "image1",
-        "title": "Heading 5",
-        "discription": "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words"
+        "title": "नाट्य Mantra",
+        "discription": "Are you ready for a theatrical experience like you've never had before? Here comes 'नाट्य Mantra', a show of one of the best skits from across India, where the performers bring their creativity to the stage and entertain you in a truly spectacular way!"
     },
     {
         "id": 6,
         "image": "image1",
-        "title": "Heading 6",
-        "discription": "Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words"
+        "title": "NRITYA SHASTRA",
+        "discription": "They say, a group dance is like a family photo: some people are smiling, some are making silly faces, and some are simply flexing their moves. Everyone is in it together,synchronizing to create memories that will last a lifetime!"
     }
     ]
