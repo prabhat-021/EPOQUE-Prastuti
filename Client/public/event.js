3e344038cc3593fe8a5fd812a8d34edb529e2fb9
aryantiwari8736 <94268382+aryantiwari8736@users.noreply.github.com>
2023-02-10 19:25:12 +0530

    error resolved

--- Client/public/event.js
+++ Client/public/event.js
@@ -5,35 +5,44 @@
         "title": "BATTLE OF BANDS",
         "imgid":"1UfZdOqrlpinNnmLeQX-gPzMFYqxHCBg0",
         "discription": "POP , R&B , Soul and Rock, What's your genre? Prastuti's 23 presents to you the biggest platform for your band to showcase your talent among the youth of the nation. Through BATTLE OF BANDS ,It's time for you to witness a musical a musical showdown lke no other!",
+        "over":"Get ready for a night filled with spell-binding live performances as bands compete to see who can bring out the most onto the stage. Join in to groove to the enthralling music of the most talented band musicians in town.",
         "featured":true
     },
     {
         "id": 2,
         "title": "RIWAYAT",
-        "imgid":"1UfZdOqrlpinNnmLeQX-gPzMFYqxHCBg0",
+        "imgid":"1i1-hmhOQ2W_l2aStUMM7_BDuEtoyEwwH",
         "title": "RIWAYAT 2",
         "discription": "Who says fashion can't be fun? A fashion even like no other is what we call RIWAYAT.Come and witness the latest trends in fashion and enjoy a night filled with colours in vogue and high style. Join us for this event where the models ace the runway in the most attractive and confident manner.",
+        "over":"Get ready for a night filled with spell-binding live performances as bands compete to see who can bring out the most onto the stage. Join in to groove to the enthralling music of the most talented band musicians in town.",
         "featured":true
     },
     {
         "id": 3,
         "title": "सुर-ए-SAAZ",
         "discription": "Get ready to unveil the singing talent of your group as Prastuti'23 presents 'सु सुरए-Saaz' a group singing event that will make you sing in rhythm and unison. Imagine a room filled with off-key notes, exaggerated harmonies,and enthusiastic performances, all coming together to create a cacophony of musical joy.",
+         "over":"Get ready for a night filled with spell-binding live performances as bands compete to see who can bring out the most onto the stage. Join in to groove to the enthralling music of the most talented band musicians in town.",
         "featured":true
     },
     {
         "id": 4,
         "title": "PRACHAND",
-        "discription": "Come adorn your character, bring your instruments, raise your voice and be ready to portray what you want to show to the crowd. Its time to shake the audience through your noble messages for the society, impress the jury, and claim the first winning prize of 'Prachand' the Nukkad Natak."
+        "imgid":"1ot1Hs7PCk_pEGxTi9wqdcY7-9eANrnIX",
+        "discription": "Come adorn your character, bring your instruments, raise your voice and be ready to portray what you want to show to the crowd. Its time to shake the audience through your noble messages for the society, impress the jury, and claim the first winning prize of 'Prachand' the Nukkad Natak.", 
+        "featured":false
     },
     {
         "id": 5,
         "title": "नाट्य Mantra",
-        "discription": "Are you ready for a theatrical experience like you've never had before? Here comes 'नाट्य Mantra', a show of one of the best skits from across India, where the performers bring their creativity to the stage and entertain you in a truly spectacular way!"
+        "imgid":"18KRotL_Fn7Aw9rIAiem_Cf26e5fgA3J6",
+        "discription": "Are you ready for a theatrical experience like you've never had before? Here comes 'नाट्य Mantra', a show of one of the best skits from across India, where the performers bring their creativity to the stage and entertain you in a truly spectacular way!",
+        "featured":false
     },
     {
         "id": 6,
         "title": "NRITYA SHASTRA",
-        "discription": "They say, a group dance is like a family photo: some people are smiling, some are making silly faces, and some are simply flexing their moves. Everyone is in it together,synchronizing to create memories that will last a lifetime!"
+        "imgid":"1mk2gBa6i3OZ99Q-JmFHLAeyLwEaQeBTI",
+        "discription": "They say, a group dance is like a family photo: some people are smiling, some are making silly faces, and some are simply flexing their moves. Everyone is in it together,synchronizing to create memories that will last a lifetime!",
+        "featured":false
     }
     ]
